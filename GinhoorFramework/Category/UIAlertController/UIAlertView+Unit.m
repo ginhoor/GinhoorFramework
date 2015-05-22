67cb86219e842daa73f391e94aae1d4dfd58001d
ginhoor <ginhoor@gmail.com>
2015-05-22 16:36:16 +0800

    删除UIAlertController，在iOS7 环境运行会崩溃

--- GinhoorFramework/Category/UIAlertController/UIAlertView+Unit.m
+++ GinhoorFramework/Category/UIAlertController/UIAlertView+Unit.m
@@ -7,7 +7,6 @@
 //
 
 #import "UIAlertView+Unit.h"
-#import "UIAlertController+Unit.h"
 #import <BlocksKit+UIKit.h>
 
 @implementation UIAlertView (Unit)
@@ -15,75 +14,32 @@
 
 + (void)alertView:(NSString *)title message:(NSString *)message cancelTitle:(NSString *)cancelTitle cancelBlock:(void(^)())cancelBlock
 {
-    Class UIAlertControllerClass = NSClassFromString(@"UIAlertController");
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:cancelTitle otherButtonTitles:nil];
+    [alert bk_setHandler:^{
+        if (cancelBlock) {
+            cancelBlock();
+        }
+    }forButtonAtIndex:0];
     
-    if (UIAlertControllerClass) {
-        
-//#warning 在presented的页面上重复present UIAlertController，会crash
-        
-        [UIAlertController defaultAlert:title
-                                message:message
-                            cancelTitle:cancelTitle
-                            cancelBlock:^(UIAlertAction *cancel) {
-                                if(cancelBlock){
-                                    cancelBlock();
-                                }
-                            }
-                         completedBlock:^{
-                                ;
-                            }];
-    } else {
-        
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:cancelTitle otherButtonTitles:nil];
-        [alert bk_setHandler:^{
-            if (cancelBlock) {
-                cancelBlock();
-            }
-        }forButtonAtIndex:0];
-        
-        [alert show];
-    }
+    [alert show];
 }
 
 
 + (void)alertView:(NSString *)title message:(NSString *)message submitTitle:(NSString *)submitTitle submitBlock:(void(^)())submitBlock cancelTitle:(NSString *)cancelTitle cancelBlock:(void(^)())cancelBlock
 {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:cancelTitle otherButtonTitles:submitTitle, nil];
+    [alert bk_setHandler:^{
+        if (cancelBlock) {
+            cancelBlock();
+        }
+    }forButtonAtIndex:0];
     
-    Class UIAlertControllerClass = NSClassFromString(@"UIAlertController");
-    
-    if (UIAlertControllerClass) {
-        
-        [UIAlertController defaultAlert:title
-                                message:message
-                            cancelTitle:cancelTitle
-                            cancelBlock:^(UIAlertAction *action) {
-                                if (cancelBlock) {
-                                    cancelBlock();
-                                }
-                            }
-                            submitTitle:submitTitle
-                            submitBlock:^(UIAlertAction *action) {
-                                if (submitBlock) {
-                                    submitBlock();
-                                }
-                            }
-                         completedBlock:^{}];
-    } else {
-
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:cancelTitle otherButtonTitles:submitTitle, nil];
-        [alert bk_setHandler:^{
-            if (cancelBlock) {
-                cancelBlock();
-            }
-        }forButtonAtIndex:0];
-        
-        [alert bk_setHandler:^{
-            if (submitBlock) {
-                submitBlock();
-            }
-        } forButtonAtIndex:1];
-        [alert show];
-    }
+    [alert bk_setHandler:^{
+        if (submitBlock) {
+            submitBlock();
+        }
+    } forButtonAtIndex:1];
+    [alert show];
 }
 
 
